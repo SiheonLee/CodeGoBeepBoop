cd077732cba0f1dbb13622cfc28407afb68594bc
Andrei <a.girjoaba@student.rug.nl>
2023-02-28 00:09:43 +0100

    max time working kinda

--- scheduler/scheduler.c
+++ scheduler/scheduler.c
@@ -3,21 +3,40 @@
 #include "MyList.h"
 #include "LibQueue.h"
 
+#define IS_SLEEPING -1
+
 List readProcess(int *n) {
     List head = NULL;
+    List tail = NULL;
     while(*n != -1) {
         List node1 = (List) malloc(sizeof(struct ListNode));
         node1->t = *n;
-        node1->next = head;
-        head=node1;
+        node1->next = NULL;
+        if (tail == NULL) {
+            head = node1;
+            tail = node1;
+        } else {
+            tail->next = node1;
+            tail = node1;
+        }
         scanf("%d", n);
     }
+    List node1 = (List) malloc(sizeof(struct ListNode));
+        node1->t = -1;
+        node1->next = NULL;
+        if (tail == NULL) {
+            head = node1;
+            tail = node1;
+        } else {
+            tail->next = node1;
+            tail = node1;
+        }
     return head;
 }
 
 List *readInput(int *nr_processes) {
     int n;
-    int size = 1;
+    int size = 100;
     List *inp = malloc(size*sizeof(List));
     scanf("%d", &n);
     while (n != EOF) {
@@ -36,6 +55,7 @@ void printOutput(List *lists, int nr_processes) {
     for (int i = 0; i < nr_processes; i++) {
         printList(lists[i]);
     }
+    printf("---\n");
 }
 
 void freeProcesses(List *processes, int nr_processes) {
@@ -45,60 +65,113 @@ void freeProcesses(List *processes, int nr_processes) {
     free(processes);
 }
 
+int safeDequeue(Queue *queue) {
+    if(isEmptyQueue(*queue)) {
+        return IS_SLEEPING;
+    }
+    return dequeue(queue);
+}
+
+bool isDoneP(List process) {
+    if(process->next->t == -1) {
+        return true;
+    }
+    return false; 
+}
+
+List *copyProcesses(List *processes, int nrP) {
+    List *copyProcesses = malloc(nrP*sizeof(List));
+    for(int i = 0; i < nrP; i++) {
+        copyProcesses[i] = processes[i];
+    }
+    return copyProcesses;
+}
+
 int main(int argc, char *argv[]) {
     int nr_processes = 0;
     List *processes = readInput(&nr_processes);
-    printOutput(processes, nr_processes);
+    List *initialProcesses = copyProcesses(processes, nr_processes);
 
         // Variable definition
-    int current_process = 0;
+    int currentP = 0;
 
-    int total_running = 0;
-    int total_blocked = 0;
+    int totalTimeUnits = 0;
 
-    int current_running = -1;
-    int current_blocked = -1;
-    Queue ready = newQueue(nr_processes);
-    Queue blocked = newQueue(nr_processes);
+    int runningP = -1;
+    int blockedP = -1;
+    Queue readyQ = newQueue(nr_processes);
+    Queue blockedQ = newQueue(nr_processes);
 
         // Execution
-    enqueue(current_process, &ready);
-    processes[current_process] = processes[current_process]->next;
-    current_process++;
-    while(!isEmptyQueue(ready) || !isEmptyQueue(blocked) || (current_running != -1) || (current_blocked != -1)) {
-        if(current_running == -1) {
-            current_running = dequeue(&ready); 
+    enqueue(currentP, &readyQ);
+    processes[currentP] = processes[currentP]->next;
+    currentP++;
+
+        // MAIN LOOP
+    while(!isEmptyQueue(readyQ) || !isEmptyQueue(blockedQ) || (runningP != IS_SLEEPING) || (blockedP != IS_SLEEPING)) {
+        printOutput(processes, nr_processes);
+
+        if(runningP == IS_SLEEPING) {
+            runningP = safeDequeue(&readyQ); 
+        }
+        if(blockedP == IS_SLEEPING) {
+            blockedP = safeDequeue(&blockedQ); 
         }
 
-        if(total_running >= processes[current_process]->t) {
-            enqueue(current_process, &ready);
-            processes[current_process] = processes[current_process]->next;
-            current_process++;
-        }
+        if(currentP <= nr_processes - 1 && totalTimeUnits >= processes[currentP]->t) {
+            enqueue(currentP, &readyQ);
+            processes[currentP] = processes[currentP]->next;
+            currentP++;
+        } else if(blockedP == IS_SLEEPING && runningP == IS_SLEEPING) {
+            printf("Sleeping everything!\n");
+        } else if(blockedP == IS_SLEEPING) {
+            printf("BLOCKED SLEEPING: %d\n", runningP);
+            totalTimeUnits += processes[runningP]->t;
+            if(!isDoneP(processes[runningP])) {
+                processes[runningP] = processes[runningP]->next;
+                enqueue(runningP, &blockedQ);
+            }
+            
+            runningP = safeDequeue(&readyQ);
+        } else if(runningP == IS_SLEEPING) {
+            printf("RUNNING SLEEPING: %d\n", blockedP);
+            totalTimeUnits += processes[blockedP]->t;
 
-            // Time pass
-        if(processes[current_running]->t < processes[current_blocked]->t) {
-            total_running += processes[current_running]->t;
-            processes[current_blocked]->t -= processes[current_running]->t;
-            total_blocked += processes[current_running]->t;
-            processes[current_running] = processes[current_running]->next;
-            enqueue(current_running, &blocked);
-            current_running = dequeue(&ready);
+            if(!isDoneP(processes[blockedP])) {
+                processes[blockedP] = processes[blockedP]->next;
+                enqueue(blockedP, &readyQ);
+            }
+            
+            blockedP = safeDequeue(&blockedQ);
+        } else if(processes[runningP]->t < processes[blockedP]->t) {
+            printf("Running less: %d\n", runningP);
+            totalTimeUnits += processes[runningP]->t;
+            processes[blockedP]->t -= processes[runningP]->t;
+
+            if(!isDoneP(processes[runningP])) {
+                processes[runningP] = processes[runningP]->next;
+                enqueue(runningP, &blockedQ);
+            }
+
+            runningP = safeDequeue(&readyQ);
         } else {
-            total_blocked += processes[current_blocked]->t;
-            processes[current_running] -= processes[current_blocked]->t;
-            total_running += processes[current_blocked]->t;
-            processes[current_blocked] = processes[current_blocked]->next;
-            enqueue(current_blocked, &ready);
-            current_blocked = dequeue(&blocked);
+            printf("Blocking less: %d\n", runningP);
+            totalTimeUnits += processes[blockedP]->t;
+            processes[runningP]->t -= processes[blockedP]->t;
+
+            if(!isDoneP(processes[blockedP])) {
+                processes[blockedP] = processes[blockedP]->next;
+                enqueue(blockedP, &readyQ);
+            }
+
+            blockedP = safeDequeue(&blockedQ);
         }
     }
+    printf("Total: %d\n", totalTimeUnits);
 
-    printf("Total: %d\n", total_running);
-
-
-    freeQueue(ready);
-    freeQueue(blocked);
-    freeProcesses(processes, nr_processes);
+    freeQueue(readyQ);
+    freeQueue(blockedQ);
+    freeProcesses(initialProcesses, nr_processes);
+    free(processes);
     return 0;
 }
